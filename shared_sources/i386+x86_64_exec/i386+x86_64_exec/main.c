7e593028292698f5fce76d8b099cc33724129e15
packagesdev <dev.packages@gmail.com>
2021-12-26 22:58:51 +0100

    Add i386 and Universal Intel simple execs and sources

---
+++ shared_sources/i386+x86_64_exec/i386+x86_64_exec/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  i386+x86_64_exec
+//
+//  Created by stephane on 12/25/21.
+//  Copyright (c) 2021 WhiteBox. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
+
---
+++ shared_sources/i386_exec/i386_exec/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  i386_exec
+//
+//  Created by stephane on 12/25/21.
+//  Copyright (c) 2021 WhiteBox. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
+
