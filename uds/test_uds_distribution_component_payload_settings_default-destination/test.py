9664dc0e45b0a6b44d4ee48e52eddb6cc2733b48
packagesdev <dev.packages@gmail.com>
2022-01-18 22:00:36 +0100

    Add uds tests for default install location

---
+++ uds/test_uds_distribution_component_payload_settings_default-destination/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+
+import os
+import errno
+import sys
+import subprocess
+import shutil
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'xar'))
+import xar
+
+import xml.etree.ElementTree as ET
+
+# Check that a component of the install location defined as the DEFAULT_DESTINATION_LAST_COMPONENT user defined setting is correctly converted during the build process
+
+test_displayed_name="uds > distribution > component > payload > settings > default destination - dynamic value"
+
+# Given
+
+user_defined_default_destination_last_component='custom'
+
+dirname = os.path.dirname(__file__)
+projectpath = os.path.join(dirname, 'test_uds_distribution_component_payload_settings_default-destination.pkgproj')
+
+# When
+
+process = subprocess.Popen(['/usr/local/bin/packagesbuild', '--project', projectpath , 'DEFAULT_DESTINATION_LAST_COMPONENT={}'.format(user_defined_default_destination_last_component)],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+
+
+# Then
+
+expected_install_location="/Applications/custom"
+
+build_directory=os.path.join(dirname, 'build')
+
+extraction_directory=os.path.join(dirname, 'extracted')
+
+xar.expandPackageToDirectory(os.path.join(build_directory, 'distribution.pkg'),extraction_directory)
+
+
+tree=ET.parse(os.path.join(extraction_directory,'package.pkg','PackageInfo'))
+
+root = tree.getroot()
+
+install_location = root.attrib['install-location']
+
+if (expected_install_location == install_location):
+	print("[+] " + test_displayed_name + ": " + '\033[1m' + "Success" + '\033[0m')
+else:
+    print("[-] " + test_displayed_name + ": " + '\033[91m' + "Failure" + '\033[0m')
+    print("    Expected: '{}' ; Found: \033[91m'{}'\033[0m".format(expected_install_location,install_location))
+
+
+
+# Cleanup
+
+shutil.rmtree(build_directory)
+shutil.rmtree(extraction_directory)
+
+sys.exit()
